62bbfe73d8a2e27444859adae4b6b0d3ac598e15
bosshogg <bosshogg@t440p.home>
2024-08-25 22:07:46 +0200

    Add chapter 8 on dependency injection

---
+++ go/learn_go_with_tests/ch_08/go.mod
@@ -0,0 +1,3 @@
+module mywriter
+
+go 1.22.6
---
+++ go/learn_go_with_tests/ch_08/greet.go
@@ -0,0 +1,15 @@
+package mywriter
+
+import (
+	"io"
+	"fmt"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+}
